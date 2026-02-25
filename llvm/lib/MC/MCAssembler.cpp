afcc275b9df53798ceb9e1d8c4939a9160ce1f78
Florian Zaruba <florian@zaruba.email>
2026-02-25 11:58:08 +0100

    Remove org size restriction

--- llvm/lib/MC/MCAssembler.cpp
+++ llvm/lib/MC/MCAssembler.cpp
@@ -247,7 +247,7 @@ uint64_t MCAssembler::computeFragmentSize(const MCFragment &F) const {
       TargetLocation += Val;
     }
     int64_t Size = TargetLocation - FragmentOffset;
-    if (Size < 0 || Size >= 0x40000000) {
+    if (Size < 0) {
       recordError(OF.getLoc(), "invalid .org offset '" + Twine(TargetLocation) +
                                    "' (at offset '" + Twine(FragmentOffset) +
                                    "')");
